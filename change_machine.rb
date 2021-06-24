c6e9f07a72b39271d3c22e1b605f66229705f5a9
dani <dani.zaghian@gmail.com>
2021-06-24 11:19:17 -0700

    testing 2

--- change_machine.rb
+++ change_machine.rb
@@ -12,5 +12,9 @@ RSpec.describe ChangeMachine do
       machine = ChangeMachine.new
       expect(machine.change(1)).to eq([1])
     end
+    it 'should return [1, 1] when given 2' do
+      machine = ChangeMachine.new
+      expect(machine.change(2)).to eq([1, 1])
+    end
   end
 end
